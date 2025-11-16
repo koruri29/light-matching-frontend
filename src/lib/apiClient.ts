555a2e4fac573da0127f939c5fd8e2b23b9ae9b6
JUNK329\kmaki <koruri821@gmail.com>
2025-11-16 15:52:05 +0900

    NEXT_PUBLIC_API_URL置き換え

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getCookieValue } from '@/utils/cookie';
 
 export const apiClient = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
+  baseURL: "/api",
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
